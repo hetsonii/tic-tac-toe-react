fdec4ef9bb57fd859fb63421d4a0e9ec032f7cd3
hetsonii <21cs068@charusat.edu.in>
2023-05-02 22:48:05 +0530

    Added moves history

--- src/App.js
+++ src/App.js
@@ -20,8 +20,22 @@ function App() {
     setNext(!next);
   };
 
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setNext(step % 2 === 0);
+  };
+
   const current = history[stepNumber];
   const winner = calculateWinner(current);
+
+  const moves = history.map((step, move) => {
+    const desc = move ? `Go to move #${move}` : 'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
 
   let status;
   if (winner) {
@@ -39,6 +53,7 @@ function App() {
       </div>
       <div className="game-info">
         <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
